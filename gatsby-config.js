1d0cd2cc3deb78f361c6747d36ecf7b6a766fafb
Vladislav Kim <vladkimkim@gmail.com>
2022-01-16 11:47:41 -0500

    Adjust Button and Seo according to kimdontdoit-gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,13 @@
+const siteUrl = /*process.env.siteUrl || */ `https://kimdontdoit.com`;
+
 module.exports = {
   siteMetadata: {
-    title: "Severus Snape",
-    titleTemplate: "%s · The Real Hero",
-    description:
-      "Hogwarts Potions master, Head of Slytherin house and former Death Eater.",
-    url: "https://admiring-goldstine-f7369a.netlify.app",
-    image: "/images/snape.jpg",
-    twitterUsername: "",
+    siteName: `The Great Gatsby`,
+    defaultTitle: `The Great Gatsby`,
+    defaultDescription: `Default meta description`,
+    author: `@kimdontdoit`,
+    siteUrl,
+    defaultImage: ``,
+    twitterUsername: `@kimdontdoit`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-sass",
-      options: {
-        additionalData: `@import "${__dirname}/src/styles/variables";`,
-        sourceMap: true,
-        useResolveUrlLoader: {
-          options: {
-            sourceMap: true,
-          },
-        },
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        //printRejected: true, // Print removed selectors and processed file names
-        purgeOnly: ["src/styles/global.scss"], // Purge only these files/folders
-      },
-    },
-  ],
 };
