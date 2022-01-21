e8545bc932973067ed28eaac193c59ef64e01c96
Vladislav Kim <vkim@o2web.ca>
2022-01-20 20:16:50 -0500

    Cursor component

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import useCursorPosition from "@kimdontdoit/the-great-gatsby-theme/src/hooks/useCursorPosition";
 import "./Cursor.scss";
 
-const Cursor = ({ image }) => {
-  if (!image) {
-    return;
-  }
-
+const Cursor = (props) => {
   const { mouseX, mouseY } = useCursorPosition();
-
+  const { image } = props;
+  console.log(image);
   return (
     <div
       className="cursor fixed"
       style={{
-        border: `2px solid black`,
         left: `${mouseX}px`,
         top: `${mouseY}px`,
       }}
-    />
+    >
+      {image && <img src={image} alt="cursor" />}
+    </div>
   );
 };
 
