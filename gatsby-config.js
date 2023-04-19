eb30d6d626b63023e854fc1c5e4ecfaac05d18ba
Vladislav Kim <vlad@kimdontdoit.com>
2023-04-19 17:41:36 -0400

    bump packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 const siteUrl = /* todo process.env.siteUrl || */ `https://kimdontdoit.com`;
 
 module.exports = {
-  siteMetadata: {
-    siteName: `The Great Gatsby`,
-    defaultTitle: `The Great Gatsby`,
-    author: `@kimdontdoit`,
-    siteUrl,
-    defaultImage: ``,
-    twitterUsername: `@kimdontdoit`,
-  },
-  plugins: [],
+    siteMetadata: {
+        siteName: `The Great Gatsby`,
+        defaultTitle: `The Great Gatsby`,
+        author: `@kimdontdoit`,
+        siteUrl,
+        defaultImage: ``,
+        twitterUsername: `@kimdontdoit`
+    },
+    plugins: []
 };
